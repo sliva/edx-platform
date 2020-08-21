d5de20c63d84655eb96ce2b769d24c7fecd42d98
Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2020-08-21 16:34:54 +0500

    Upgrade django-ipware

--- openedx/core/djangoapps/util/ratelimit.py
+++ openedx/core/djangoapps/util/ratelimit.py
@@ -3,8 +3,8 @@ Code to get ip from request.
 """
 
 
-from ipware.ip import get_ip
+from ipware.ip import get_client_ip
 
 
 def real_ip(group, request):  # lint-amnesty, pylint: disable=unused-argument
-    return get_ip(request)
+    return get_client_ip(request)[0]
