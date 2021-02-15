987d5ff19d9ece9ae856b0475b8e66ce9afefe08
usamasadiq <usama.sadiq@arbisoft.com>
2021-02-15 14:56:20 +0500

    Ran pyupgrade on lms/djangoapps/coursearehistoryextended

--- lms/djangoapps/coursewarehistoryextended/models.py
+++ lms/djangoapps/coursewarehistoryextended/models.py
@@ -14,14 +14,13 @@ ASSUMPTIONS: modules have unique IDs, even across different module_types
 """
 
 
-import six
 from django.db import models
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from django.utils.encoding import python_2_unicode_compatible
 
-from lms.djangoapps.courseware.models import BaseStudentModuleHistory, StudentModule
 from lms.djangoapps.courseware.fields import UnsignedBigIntAutoField
+from lms.djangoapps.courseware.models import BaseStudentModuleHistory, StudentModule
 
 
 @python_2_unicode_compatible
@@ -33,7 +32,7 @@ class StudentModuleHistoryExtended(BaseStudentModuleHistory):
     This new extended CSMH has a larger primary key that won't run out of space
     so quickly."""
 
-    class Meta(object):
+    class Meta:
         app_label = 'coursewarehistoryextended'
         get_latest_by = "created"
         index_together = ['student_module']
@@ -67,4 +66,4 @@ class StudentModuleHistoryExtended(BaseStudentModuleHistory):
         StudentModuleHistoryExtended.objects.filter(student_module=instance).all().delete()
 
     def __str__(self):
-        return six.text_type(repr(self))
+        return str(repr(self))
